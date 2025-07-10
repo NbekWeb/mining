1448fb7be581f22e90d1a133e37955816e2f78a4
NbekWeb <nbekdev@gmail.com>
2025-07-10 15:35:04 +0500

    change default route

--- src/components/StartFree.vue
+++ src/components/StartFree.vue
@@ -4,7 +4,7 @@
     :class="{ 'active': isActive }"
   >
     <div
-      class="h-full relative flex items-center gap-2 pl-4 max-lg:bg-white max-sm:h-15 max-sm:rounded-md max-sm:border-fiolet group-hover:border-transparent border-l-2 border-y-2 border-fiolet rounded-l-lg"
+      class="h-full box-effect-sm relative flex items-center gap-2 pl-4 max-lg:bg-white max-sm:h-15 max-sm:rounded-md max-sm:border-fiolet group-hover:border-transparent border-l-2 border-y-2 max-sm:border-2 border-fiolet rounded-l-lg"
       :class="isActive && 'border-transparent'"
     >
       <span> visitb.io/ </span>
@@ -67,5 +67,14 @@ const isActive = ref(false);
   .box-effect.active button {
     border-right: 1px solid #06ab4f;
   }
+}
+@media (max-width: 640px) {
+  .box-effect-sm:hover,
+  .box-effect-sm:focus,
+  .box-effect-sm.active {
+    box-shadow: 0 0 0 2px #06ab4f;
+    border-color: transparent;
+  }
+
 }
 </style>
