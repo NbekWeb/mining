7b3bea0fdf036b50b9e28ae5635daef1fcc670ec
NbekWeb <nbekdev@gmail.com>
2025-08-09 10:46:37 +0500

    s

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_APP_BASE_URL}`,
+  baseURL: `https://mining-amirates.us.com/api/`,
 });
 
 export const api = ({ url, open = false, ...props }) => {
